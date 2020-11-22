8d6debacf2e6723d3df89e2e9ffd133eaab16766
Isacco Borsani <isacco.borsani@gmail.com>
2020-11-22 11:27:07 +0100

    init project 100%

--- app.py
+++ app.py
@@ -6,8 +6,3 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-@app.route('/<word>', defaults={'word': 'bird'})
-def word_up(word):
-    return render_template('whatstheword.html', word=word)
