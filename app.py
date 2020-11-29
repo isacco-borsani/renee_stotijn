5f8e0d50db5275170fdb7e0b0a9941875b02d2ee
Isacco Borsani <isacco.borsani@gmail.com>
2020-11-29 18:31:03 +0100

    fix

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from os import listdir
+from os.path import isfile, join
 
 app = Flask(__name__)
 
@@ -6,3 +8,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/get_paints', methods=['GET'])
+def get_paints():
+    try:
+        file_paints = [str(f) for f in listdir('static/images/quadri/') if isfile(join('static/images/quadri', f))]
+    except FileNotFoundError:
+        return {'ret': False, 'content': None}
+    return {'ret': True, 'content': file_paints}
