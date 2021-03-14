fd6f5417ab43d684c0c6e8e3d29ac4777b5e6acb
Isacco Borsani <isacco@DESKTOP-UBULPPO.homenet.telecomitalia.it>
2021-03-14 18:55:10 +0100

    translations system implemented

--- app.py
+++ app.py
@@ -1,8 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 from os import listdir
 from os.path import isfile, join
+from flask_babel import Babel, gettext
 
 app = Flask(__name__)
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['LANGUAGES'] = {
+    'en': 'English',
+    'nl': 'Dutch'
+}
+
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+babel = Babel(app)
+
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+
+
+@babel.localeselector
+def get_locale():
+    # if the user has set up the language manually it will be stored in the session,
+    # so we use the locale from the user settings
+    try:
+        language = session['language']
+    except KeyError:
+        language = "en"
+    if language is not None:
+        return language
+    return request.accept_languages.best_match(app.config['LANGUAGES'].keys())
+
+
+@app.context_processor
+def inject_conf_var():
+    return dict(
+        AVAILABLE_LANGUAGES=app.config['LANGUAGES'],
+        CURRENT_LANGUAGE=session.get('language', request.accept_languages.best_match(app.config['LANGUAGES'].keys())))
+
+
+@app.route('/language/<language>')
+def set_language(language=None):
+    if language in app.config['LANGUAGES']:
+        session['language'] = language
+    else:
+        redirect(url_for('index'))
+    return redirect(request.referrer)
 
 
 @app.route('/')
@@ -14,6 +55,7 @@ def index():
 def get_paints():
     try:
         file_paints = [str(f) for f in listdir('static/images/quadri/') if isfile(join('static/images/quadri', f))]
+        print(file_paints)
     except FileNotFoundError:
         return {'ret': False, 'content': None}
     return {'ret': True, 'content': file_paints}
--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     }).done(function (data) {
         console.log(data['content'].length);
         $('.tiles__line-img').each(function () {
+            console.log('background-image', 'url(static/images/quadri/' + data['content'][Math.floor(Math.random() * data['content'].length)] + ')')
             $(this).css('background-image', 'url(static/images/quadri/' + data['content'][Math.floor(Math.random() * data['content'].length)] + ')')
         })
     });
